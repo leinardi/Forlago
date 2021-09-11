38db3bc1b7623c72ed85415c9bd6d794391cb4d5
Roberto Leinardi <leinardi@gmail.com>
2021-09-11 20:53:48 +0200

    Added SavedStateHandle to viewmodel

--- modules/feature-home/src/main/kotlin/com/leinardi/androidtemplateproject/home/ui/HomeViewModel.kt
+++ modules/feature-home/src/main/kotlin/com/leinardi/androidtemplateproject/home/ui/HomeViewModel.kt
@@ -2,23 +2,31 @@ package com.leinardi.androidtemplateproject.home.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
 
     val counterLiveData: LiveData<Int>
         get() = counter
 
     private val counter = MutableLiveData<Int>()
-    private var count = 0
+    private var count = savedStateHandle.get<Int>(SAVED_COUNT) ?: 0
 
     fun increaseCounter() {
         Timber.d("increaseCounter")
         counter.value = ++count
+        savedStateHandle.set(SAVED_COUNT, count)
+    }
+
+    companion object {
+        private const val SAVED_COUNT = "saved_count"
     }
 
 }
