56897e724303a61b1d0f8bdaecb6f7d22ab974fa
Javier Arroyo <javier.arroyo@veeva.com>
2023-06-15 15:20:26 +0200

    Task: Templates - Add Generate library module

---
+++ modules/.module-template/library/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("forlago.android-library-conventions")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace ="com.leinardi.forlago.library.@placeholderlowercase@"
+    resourcePrefix = "@placeholder_snake_case@_"
+    defaultConfig {
+        consumerProguardFiles("$projectDir/proguard-@placeholderlowercase@-consumer-rules.pro")
+    }
+}
+
+dependencies {
+    implementation(projects.modules.library@PlaceholderName@Api)
+    implementation(libs.hilt.android)
+
+    kapt(libs.hilt.compiler)
+}
